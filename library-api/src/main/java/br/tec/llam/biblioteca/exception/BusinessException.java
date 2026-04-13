47f880ee6951588e3c6881a00d6c98f33df6b061
Leandro Leal <leandro.leal@llam.tec.br>
2026-04-13 10:54:41 -0300

    Limpando tudo.

--- library-api/src/main/java/br/tec/llam/biblioteca/exception/BusinessException.java
+++
@@ -1,8 +0,0 @@
-package br.tec.llam.biblioteca.exception;
-
-public class BusinessException extends RuntimeException {
-    
-    public BusinessException(String message) {
-        super(message);
-    }
-}
